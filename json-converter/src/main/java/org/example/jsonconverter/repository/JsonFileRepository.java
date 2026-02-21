fd60c54f24e2c8f133ac8d15bce8b8fe39729a13
Arjun <arjunbhaliya2004@gmail.com>
2026-02-20 19:14:17 -0500

    remove json-converter task

--- json-converter/src/main/java/org/example/jsonconverter/repository/JsonFileRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.jsonconverter.repository;
-
-import org.example.jsonconverter.entity.JsonFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JsonFileRepository extends JpaRepository<JsonFile,Long> {
-    JsonFile findByFileName(String filename);
-}
